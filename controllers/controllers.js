9f333ee0fa9aee06e07b677dba3038f32263547e
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-19 14:34:25 -0400

    new

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -14,9 +14,34 @@ accountRouter.get('/', function(req, res) {
 accountRouter.get('/new', function(req, res) {
     res.send(accountsApi.createAccount());
   })
+  accountRouter.get('/:accountId', function(req,res){
+    accountsApi.getAccount(req.params.accountId)
+      .then(account => {
+        res.send(account)
+      })
+  })
 
+  accountRouter.post('/', function(req, res){
+    accountsApi.addAccount(req.body)
+      .then(() => {
+        res.send('Account created');
+      })
+  })
 
-  
+  accountRouter.put('/:accountId', function(req,res){
+    accountsApi.updateAccount(req.params.accountId, req.body)
+      .then(() => {
+        res.send('Account updated');
+      })
+  })
+
+  accountRouter.delete('/:accountId', function(req,res){
+    accountsApi.deleteAccount(req.params.accountId)
+      .then(() => {
+        res.send('Account deleted');
+      })
+  })
+
   module.exports = {
       accountRouter
   }

02e547378557e3fa3a901ac86f9019c997944fef
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-19 12:14:59 -0400

    save

--- controllers.js
+++ controllers.js
@@ -1,1 +1,22 @@
-a new file
+const express = require ('express')
+const accountAPI = require ('../models/model.js')
+const accountRouter = express.Router()
+
+
+accountRouter.get('/', function(req, res) {
+    accountsApi.getAccounts()
+      .then(accounts => {
+        res.send(accounts)
+      })
+  })
+  
+
+accountRouter.get('/new', function(req, res) {
+    res.send(accountsApi.createAccount());
+  })
+
+
+  
+  module.exports = {
+      accountRouter
+  }

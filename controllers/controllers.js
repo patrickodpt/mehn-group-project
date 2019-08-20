10a6d9071bd4a77da3d17ccd0507782a8c1aab17
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-20 07:29:50 -0400

    fixing bugs

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,47 +1,50 @@
-const express = require ('express')
-const accountsApi = require ('../models/model.js')
+const express = require('express')
+const accountsApi = require('../models/model.js')
 const accountRouter = express.Router()
 
 
-accountRouter.get('/', function(req, res) {
-    accountsApi.getAccounts()
-      .then(accounts => {
-        res.send(accounts)
-      })
-  })
+accountRouter.get('/', function (req, res) {
+  accountsApi.getAccounts()
+    .then(accounts => {
+      res.send(accounts)
+    })
+})
 
 
-accountRouter.get('/new', function(req, res) {
-    res.send(accountsApi.createBlankAccount());
-  })
-  accountRouter.get('/:accountId', function(req,res){
-    accountsApi.getAccount(req.params.accountId)
-      .then(account => {
-        res.send(account)
-      })
-  })
+accountRouter.get('/new', function (req, res) {
+  res.send(accountsApi.createBlankAccount());
+})
 
-  accountRouter.post('/', function(req, res){
-    accountsApi.addAccount(req.body)
-      .then(() => {
-        res.send('Account created');
-      })
-  })
 
-  accountRouter.put('/:accountId', function(req,res){
-    accountsApi.updateAccount(req.params.accountId, req.body)
-      .then(() => {
-        res.send('Account updated');
-      })
-  })
 
-  accountRouter.delete('/:accountId', function(req,res){
-    accountsApi.deleteAccount(req.params.accountId)
-      .then(() => {
-        res.send('Account deleted');
-      })
-  })
+accountRouter.get('/:accountId', function (req, res) {
+  accountsApi.getAccount(req.params.id)
+    .then((account) => {
+      res.send(account)
+    })
+})
 
-  module.exports = {
-      accountRouter
-  }
+accountRouter.post('/', function (req, res) {
+  accountsApi.addAccount(req.body)
+    .then(() => {
+      res.send('Account created');
+    })
+})
+
+accountRouter.put('/:accountId', function (req, res) {
+  accountsApi.updateAccount(req.params.accountId, req.body)
+    .then(() => {
+      res.send('Account updated');
+    })
+})
+
+accountRouter.delete('/:accountId', function (req, res) {
+  accountsApi.deleteAccount(req.params.accountId)
+    .then(() => {
+      res.send('Account deleted');
+    })
+})
+
+module.exports = {
+  accountRouter
+}
